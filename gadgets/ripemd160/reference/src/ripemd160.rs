0dc93a499ad8e624bffa27f7bf39291f6c5eb715
Saravanan Vijayakumaran <sarva.v@gmail.com>
2022-08-31 14:00:55 +0530

    Changing types of handle endianness

--- gadgets/ripemd160/reference/src/ripemd160.rs
+++ gadgets/ripemd160/reference/src/ripemd160.rs
@@ -10,15 +10,48 @@ pub struct State {
     e: u32,
 }
 
+#[derive(Clone, Copy)]
+pub struct MessageBlock([u32; BLOCK_SIZE]);
+
+impl MessageBlock {
+    pub fn get_word(&self, index: usize) -> u32 {
+        self.0[index]
+    }
+}
+
 impl From<[u32; DIGEST_SIZE]> for State {
     fn from(s: [u32; DIGEST_SIZE]) -> Self {
        State { a: s[0], b: s[1], c: s[2], d: s[3], e: s[4] } 
     }
 }
 
-impl From<State> for [u32; DIGEST_SIZE] {
+// impl From<State> for [u32; DIGEST_SIZE] {
+//     fn from(s: State) -> Self {
+//         [s.a.to_be(), s.b.to_be(), s.c.to_be(), s.d.to_be(), s.e.to_be()]
+//     }
+// }
+
+impl From<State> for [u8; DIGEST_SIZE_BYTES] {
     fn from(s: State) -> Self {
-        [s.a.to_be(), s.b.to_be(), s.c.to_be(), s.d.to_be(), s.e.to_be()]
+        [   
+            s.a.to_le_bytes(),
+            s.b.to_le_bytes(),
+            s.c.to_le_bytes(), 
+            s.d.to_le_bytes(),
+            s.e.to_le_bytes()
+        ].concat().try_into().expect("Failed conversion")
+    }
+}
+
+impl From<[u8; BLOCK_SIZE_BYTES]> for MessageBlock {
+    fn from(s: [u8; BLOCK_SIZE_BYTES]) -> Self {
+        assert!(s.len() == BLOCK_SIZE_BYTES);
+        let mut v: Vec<u32> = vec![];
+        for i in 0..BLOCK_SIZE {
+            v.push(u32::from_le_bytes([s[4*i], s[4*i+1], s[4*i+2], s[4*i+3]]));
+        }
+        let a = v.as_slice();
+        MessageBlock(a.try_into().expect("Incorrect length"))
     }
 }
 
@@ -28,10 +61,10 @@ const ROUND_FUNC_RIGHT: [fn(u32,u32,u32) -> u32; 5] = [f5, f4, f3, f2, f1];
 pub fn left_step(
     round_idx: usize,
     s: State,
-    msg_block: [u32; BLOCK_SIZE],
+    msg_block: MessageBlock,
 ) -> State {
     let f = ROUND_FUNC_LEFT[round_idx/ROUND_PHASE_SIZE];
-    let m = msg_block[MSG_SEL_IDX_LEFT[round_idx]];
+    let m = msg_block.get_word(MSG_SEL_IDX_LEFT[round_idx]);
     let k = ROUND_CONSTANTS_LEFT[round_idx/ROUND_PHASE_SIZE];
     let shift = ROL_AMOUNT_LEFT[round_idx];
     let t =
@@ -54,10 +87,10 @@ pub fn left_step(
 pub fn right_step(
     round_idx: usize,
     s: State,
-    msg_block: [u32; BLOCK_SIZE],
+    msg_block: MessageBlock
 ) -> State {
     let f = ROUND_FUNC_RIGHT[round_idx/ROUND_PHASE_SIZE];
-    let m = msg_block[MSG_SEL_IDX_RIGHT[round_idx]];
+    let m = msg_block.get_word(MSG_SEL_IDX_RIGHT[round_idx]);
     let k = ROUND_CONSTANTS_RIGHT[round_idx/ROUND_PHASE_SIZE];
     let shift = ROL_AMOUNT_RIGHT[round_idx];
     let t =
@@ -103,8 +136,8 @@ pub fn combine_left_right_states(
 
 pub fn compress(
     s: State,
-    msg_block: [u32; BLOCK_SIZE],
-) -> [u32; DIGEST_SIZE] {
+    msg_block: MessageBlock,
+) -> [u8; DIGEST_SIZE_BYTES] {
     let mut left_state = s;
     let mut right_state = s;
     for j in 0..ROUNDS {
@@ -119,8 +152,8 @@ pub fn compress(
 }
 
 pub fn compress_first_block(
-    msg_block: [u32; BLOCK_SIZE],
-) -> [u32; DIGEST_SIZE] {
+    msg_block: MessageBlock,
+) -> [u8; DIGEST_SIZE_BYTES] {
     compress(INITIAL_VALUES.into(), msg_block)
 }
 
@@ -128,19 +161,19 @@ pub fn compress_first_block(
 mod tests {
     use crate::constants::*;
     use crate::helper_functions::*;
-    use crate::ripemd160::right_step;
-    use super::compress_first_block;
-    use super::{left_step, State};
+    use super::{left_step, right_step, compress_first_block, MessageBlock, State};
     use rand::Rng;
 
     #[test]
     fn test_left_step () {
         let mut rng = rand::thread_rng();
-        let msg_block: [u32; BLOCK_SIZE] = rng.gen();
+        let mut msg_block_bytes = [0_u8; BLOCK_SIZE_BYTES];
+        rng.fill(&mut msg_block_bytes);
+        let msg_block: MessageBlock = msg_block_bytes.into();
 
         let s = State::default();
         let mut s_next = State::default();
-        let sum = msg_block[0].overflowing_add(ROUND_CONSTANTS_LEFT[0]);
+        let sum = msg_block.get_word(0).overflowing_add(ROUND_CONSTANTS_LEFT[0]);
         s_next.b = rol(sum.0, ROL_AMOUNT_LEFT[0]);
 
         assert_eq!(left_step(0, s, msg_block), s_next);
@@ -149,11 +182,15 @@ mod tests {
     #[test]
     fn test_right_step () {
         let mut rng = rand::thread_rng();
-        let msg_block: [u32; BLOCK_SIZE] = rng.gen();
+        let mut msg_block_bytes = [0_u8; BLOCK_SIZE_BYTES];
+        rng.fill(&mut msg_block_bytes);
+        let msg_block: MessageBlock = msg_block_bytes.into();
 
         let s = State::default();
         let mut s_next = State::default();
-        let sum = msg_block[5].overflowing_add(0xFFFF_FFFF).0.overflowing_add(ROUND_CONSTANTS_RIGHT[0]);
+        let sum = msg_block.get_word(5)
+                                .overflowing_add(0xFFFF_FFFF).0
+                                .overflowing_add(ROUND_CONSTANTS_RIGHT[0]);
         s_next.b = rol(sum.0, ROL_AMOUNT_RIGHT[0]);
 
         assert_eq!(right_step(0, s, msg_block), s_next);
@@ -161,6 +198,6 @@ mod tests {
 
     #[test]
     fn test_compress_first_block () {
-        assert_eq!(compress_first_block(PADDED_TEST_INPUT), TEST_INPUT_HASH);
+        assert_eq!(compress_first_block(PADDED_TEST_INPUT.into()), TEST_INPUT_HASH);
     }
 }
